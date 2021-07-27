6a23e88bbcebf2bf5115b33205f7ede1190cc132
Ali Nekoh <60889389+aliniko@users.noreply.github.com>
2021-07-27 17:38:45 +0200

    added component with logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
+import Header from './component/ui/Header';
 import './App.css';
 
 const App = () => {
   return (
     <div className="container">
-      Hello
+      <Header />
     </div>
   );
 }
