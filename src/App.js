b296899e456da9d35976afa412aa39fd5618ad57
Ali Nekoh <60889389+aliniko@users.noreply.github.com>
2021-07-27 18:12:09 +0200

    fetched data with API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
 import Header from './component/ui/Header';
 import './App.css';
 
 const App = () => {
+ 
+ const [items, setItems] = useState([])
+ const [isLoading, setIsLoading] = useState(true)
+
+ useEffect(() =>{
+  const fetchItems = async () => { 
+    const result = await  axios(`https://www.breakingbadapi.com/api/characters`)
+ 
+    console.log(result.data)
+
+    setItems(result.data)
+    setIsLoading(false)
+
+  }
+  
+  fetchItems()
+ }, [])
+ 
   return (
     <div className="container">
       <Header />
